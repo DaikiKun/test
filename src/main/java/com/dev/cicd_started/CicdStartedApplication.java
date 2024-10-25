ec1f85ed57fe1210e3b889d68d012c43208b544c
oak soe khant <63006109+DaikiKun@users.noreply.github.com>
2024-10-25 23:06:27 +0630

    initial commit

---
+++ src/main/java/com/dev/cicd_started/CicdStartedApplication.java
@@ -0,0 +1,13 @@
+package com.dev.cicd_started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdStartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/cicd_started/CicdStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.cicd_started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
